a3f5d6cdba6efbe9d392cfb74ad3b49c07461766
Jenkins <jenkins@bqbot.bq.local>
2016-08-01 15:33:19 +0200

    Version 1.0.1_20150610-1147 released

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -8176,6 +8176,117 @@ free_hdd_ctx:
    hdd_set_ssr_required (VOS_FALSE);
 }
 
+/*wangxun*/
+static VOS_STATUS hdd_update_wifi_mac(hdd_context_t* pHddCtx)
+{
+	struct file *fp      = NULL;
+	char* filepath       = "/persist/softmac";
+	char macbuf[20]      ={0};
+	int ret              = 0;
+	mm_segment_t oldfs   = {0};
+	char random_mac[20]  = {0};
+	unsigned int softmac[6];
+
+	//first output random wifi mac address.
+	random_mac[0] = 0x00; /* locally administered */
+	random_mac[1] = 0x23;
+	random_mac[2] = 0xB1;
+	random_mac[3] = 0x11;//random32() & 0xff;
+	random_mac[4] = 0x22;//random32() & 0xff;
+	random_mac[5] = 0x33;//random32() & 0xff;
+		
+	fp = filp_open(filepath, O_RDWR, 0666);
+	if(IS_ERR(fp)) {
+		printk("[WIFI] %s: File open error\n", filepath);
+		return VOS_STATUS_E_FAILURE;
+	}
+	
+	oldfs = get_fs();
+	set_fs(get_ds());    
+
+	if(fp->f_mode & FMODE_READ) {
+		ret = fp->f_op->read(fp, (char *)macbuf, 17, &fp->f_pos);
+		if(ret < 0)
+			printk("[WIFI] Mac address [%s] Failed to read from File: %s\n", macbuf, filepath);
+		else
+			printk("[WIFI] Mac address [%s] read from File: %s\n", macbuf, filepath);
+	}
+	
+	set_fs(oldfs);
+
+	if (fp)
+	    filp_close(fp, NULL);
+		
+	if (sscanf(macbuf, "%02x:%02x:%02x:%02x:%02x:%02x",
+								&softmac[0], &softmac[1], &softmac[2],
+								&softmac[3], &softmac[4], &softmac[5])==6) 
+	{
+		if (memcmp(macbuf, "00:00:00:00:00:00", 17) != 0){
+			int i;
+			for (i = 0; i < 6; i++)
+			{
+			   pHddCtx->cfg_ini->intfMacAddr[0].bytes[i]= softmac[i] & 0xff;
+			}
+	        printk("wifi mac is =%02X:%02X:%02X:%02X:%02X:%02X\n",
+	            pHddCtx->cfg_ini->intfMacAddr[0].bytes[0], 
+	            pHddCtx->cfg_ini->intfMacAddr[0].bytes[1],
+	            pHddCtx->cfg_ini->intfMacAddr[0].bytes[2], 
+	            pHddCtx->cfg_ini->intfMacAddr[0].bytes[3], 
+	            pHddCtx->cfg_ini->intfMacAddr[0].bytes[4], 
+	            pHddCtx->cfg_ini->intfMacAddr[0].bytes[5]);
+		}else{		 
+			struct file *fp      = NULL;
+			char mac_in[20]      ={0};
+			int ret              = 0;
+			mm_segment_t oldfs   = {0};
+			printk("wxun: has softmac file but mac address is invalid(00:00:00:00:00:00), so write random mac address to softmac file.\n");
+			fp = filp_open(filepath, O_RDWR | O_CREAT, 0666);
+	    	if(IS_ERR(fp)) {
+	    		printk("[WIFI] %s: File open error\n", filepath);
+	    		return VOS_STATUS_E_FAILURE;
+	    	}
+	    	
+			oldfs = get_fs();
+			set_fs(get_ds());
+	        
+	        printk("random_mac is =%02X:%02X:%02X:%02X:%02X:%02X\n",
+	            random_mac[0], random_mac[1],random_mac[2], random_mac[3], random_mac[4], random_mac[5]);
+
+	    	if(fp->f_mode & FMODE_WRITE) {			
+			sprintf(mac_in,"%02x:%02x:%02x:%02x:%02x:%02x",
+	                     random_mac[0], random_mac[1], random_mac[2],
+	                     random_mac[3], random_mac[4], random_mac[5]);
+	    		ret = fp->f_op->write(fp, (const char *)mac_in, 17, &fp->f_pos);
+	    		if(ret < 0)
+	    			printk("[WIFI] Mac address [%s] Failed to write into File: %s\n", mac_in, filepath);
+	    		else
+	    			printk("[WIFI] Mac address [%s] written into File: %s\n", mac_in, filepath);
+	    	}
+	    	
+			set_fs(oldfs);
+
+			if (fp)
+			    filp_close(fp, NULL);
+
+			if (sscanf(mac_in, "%02x:%02x:%02x:%02x:%02x:%02x",
+								&softmac[0], &softmac[1], &softmac[2],
+								&softmac[3], &softmac[4], &softmac[5])==6) 
+			{
+				int i;
+				for (i = 0; i < 6; i++)
+				{
+				   pHddCtx->cfg_ini->intfMacAddr[0].bytes[i]= softmac[i] & 0xff;
+				}				
+			}
+				
+	    }
+	}
+
+	return VOS_STATUS_SUCCESS;
+	
+}
+
+/*end*/
 
 /**---------------------------------------------------------------------------
 
@@ -8825,6 +8936,7 @@ int hdd_wlan_startup(struct device *dev )
           goto err_free_hdd_context;
       }
       hddLog(VOS_TRACE_LEVEL_FATAL,"%s: FTM driver loaded success fully",__func__);
+	  pHddCtx->isLoadUnloadInProgress = WLAN_HDD_NO_LOAD_UNLOAD_IN_PROGRESS;
 
       vos_set_load_unload_in_progress(VOS_MODULE_ID_VOSS, FALSE);
       return VOS_STATUS_SUCCESS;
@@ -10550,7 +10662,7 @@ int wlan_hdd_scan_abort(hdd_adapter_t *pAdapter)
     if (pScanInfo->mScanPending)
     {
         abortScanStatus = hdd_abort_mac_scan(pHddCtx, pScanInfo->sessionId,
-                                             eCSR_SCAN_ABORT_DEFAULT);
+                                    eCSR_SCAN_ABORT_DEFAULT);
         VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO,
                   FL("abortScanStatus: %d"), abortScanStatus);
 
@@ -10561,15 +10673,15 @@ int wlan_hdd_scan_abort(hdd_adapter_t *pAdapter)
         if (abortScanStatus == eSIR_ABORT_ACTIVE_SCAN_LIST_NOT_EMPTY)
         {
             INIT_COMPLETION(pScanInfo->abortscan_event_var);
-            status = wait_for_completion_interruptible_timeout(
+        status = wait_for_completion_interruptible_timeout(
                            &pScanInfo->abortscan_event_var,
                            msecs_to_jiffies(5000));
-            if (0 >= status)
-            {
-               VOS_TRACE( VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+        if (0 >= status)
+        {
+           VOS_TRACE( VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
                   "%s: Timeout or Interrupt occurred while waiting for abort"
                   "scan, status- %ld", __func__, status);
-                return -ETIMEDOUT;
+            return -ETIMEDOUT;
             }
         }
         else if (abortScanStatus == eSIR_ABORT_SCAN_FAILURE)
